ad4520a0406bcd898bc6900a6469a1511bc82dbf
Adane <adanemoges6@gmail.com>
2024-02-14 09:40:31 +0300

    pycode fixed for basic auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -11,9 +11,13 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """basic auth class"""
 
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(
+        self, authorization_header: str
+    ) -> str:
         """extract_base64_authorization_header"""
-        if authorization_header is None or type(authorization_header) is not str:
+        if authorization_header is None or type(
+            authorization_header
+        ) is not str:
             return None
         if not authorization_header.startswith("Basic "):
             return None
@@ -29,7 +33,9 @@ class BasicAuth(Auth):
         ):
             return None
         try:
-            return base64.b64decode(base64_authorization_header).decode("utf-8")
+            return base64.b64decode(
+                base64_authorization_header
+            ).decode("utf-8")
         except Exception:
             return None
 
