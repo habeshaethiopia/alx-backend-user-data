73086af01be56ea0a56dce691c7608a1e21d3c04
Adane <adanemoges6@gmail.com>
2024-02-17 05:56:45 +0300

    logout

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -32,3 +32,16 @@ def login() -> str:
     SESSION_NAME = os.getenv("SESSION_NAME")
     response.set_cookie(SESSION_NAME, session_id)
     return response
+
+
+@app_views.route("auth_session/logout", methods=["DELETE"], strict_slashes=False)
+def logout() -> str:
+    """DELETE /api/v1/auth_session/logout
+    Return:
+      - the status of the API
+    """
+    from api.v1.app import auth
+
+    if auth.destroy_session(request):
+        return jsonify({}), 200
+    abort(404)
