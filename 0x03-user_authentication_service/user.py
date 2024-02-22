93c24c4d282fb6fff5598f037250460e59100ec5
Adane <adanemoges6@gmail.com>
2024-02-22 06:17:08 +0300

    task 0

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""SQLAlchemy model"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class User(Base):
+    """User class"""
+
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
+
+    # def __str__(self):
+    #     """String representation of the User object"""
+    #     return "User: {} {}".format(self.first_name, self.last_name)
+    # def __repr__(self):
+    #     """String representation of the User object"""
+    #     return "User: {} {}".format(self.first_name, self.last_name)
+    def to_dict(self):
+        """Return a dictionary representation of the object"""
+        return {
+            "id": self.id,
+            "email": self.email,
+            "hashed_password": self.hashed_password,
+            "reset_token": self.reset_token,
+        }
+
+    def from_dict(self, data):
+        """Set the object's attributes from a dictionary"""
+        for key, value in data.items():
+            setattr(self, key, value)
