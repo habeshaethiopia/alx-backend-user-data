6247647007ba1ea22371b2c944a4529e8e28f1cc
Adane <adanemoges6@gmail.com>
2024-02-22 10:00:43 +0300

    db

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,13 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import NoResultFound, InvalidRequestError
 
-from user import User
-
-from user import Base
-
-import bcrypt
+from user import Base, User
 
 
 class DB:
@@ -25,37 +20,16 @@ class DB:
         self.__session = None
 
     @property
-    def session(self) -> Session:
+    def _session(self) -> Session:
         """Memoized session object"""
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email, hashed_password) -> User:
+    def add_user(self, email: str, hashed_password: str) -> User:
         """Add a user to the database"""
         user = User(email=email, hashed_password=hashed_password)
-        self.session.add(user)
-        self.session.commit()
+        self._session.add(user)
+        self._session.commit()
         return user
-
-    def find_user_by(self, **kwargs) -> User:
-        """Find a user by a given attribute"""
-        try:
-            user = self.session.query(User).filter_by(**kwargs).first()
-            if not user:
-                raise NoResultFound
-            return user
-        except InvalidRequestError:
-            return None
-
-    def update_user(self, user_id, **kwargs) -> None:
-        """Update a user in the database"""
-
-        user = self.find_user_by(id=user_id)
-        if not user:
-            raise ValueError("No user found with the given id")
-
-        for key, value in kwargs.items():
-            setattr(user, key, value)
-        self.__session.commit()
--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -8,7 +8,13 @@ Base = declarative_base()
 
 class User(Base):
     """User class"""
-
+    cnt=0
+    def __init__(self, email: str, hashed_password: str):
+        """Initialize a new User instance"""
+        self.email = email
+        self.hashed_password = hashed_password
+        User.cnt += 1
+        self.id = User.cnt
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
@@ -22,16 +28,16 @@ class User(Base):
     # def __repr__(self):
     #     """String representation of the User object"""
     #     return "User: {} {}".format(self.first_name, self.last_name)
-    def to_dict(self):
-        """Return a dictionary representation of the object"""
-        return {
-            "id": self.id,
-            "email": self.email,
-            "hashed_password": self.hashed_password,
-            "reset_token": self.reset_token,
-        }
+    # def to_dict(self):
+    #     """Return a dictionary representation of the object"""
+    #     return {
+    #         "id": self.id,
+    #         "email": self.email,
+    #         "hashed_password": self.hashed_password,
+    #         "reset_token": self.reset_token,
+    #     }
 
-    def from_dict(self, data):
-        """Set the object's attributes from a dictionary"""
-        for key, value in data.items():
-            setattr(self, key, value)
+    # def from_dict(self, data):
+    #     """Set the object's attributes from a dictionary"""
+    #     for key, value in data.items():
+    #         setattr(self, key, value)
