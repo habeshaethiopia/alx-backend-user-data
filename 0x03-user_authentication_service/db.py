02ecc33f0ee34cf6c76bdaa310815cb3fcd0e038
Adane <adanemoges6@gmail.com>
2024-02-22 09:16:53 +0300

    task 1

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.orm.exc import NoResultFound, InvalidRequestError
+from sqlalchemy.exc import NoResultFound, InvalidRequestError
 
 from user import User
 
@@ -23,9 +23,8 @@ class DB:
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
-
     @property
-    def __session(self) -> Session:
+    def session(self) -> Session:
         """Memoized session object"""
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
@@ -35,14 +34,14 @@ class DB:
     def add_user(self, email, hashed_password) -> User:
         """Add a user to the database"""
         user = User(email=email, hashed_password=hashed_password)
-        self.__session.add(user)
-        self.__session.commit()
+        self.session.add(user)
+        self.session.commit()
         return user
 
     def find_user_by(self, **kwargs) -> User:
         """Find a user by a given attribute"""
         try:
-            user = self.__session.query(User).filter_by(**kwargs).first()
+            user = self.session.query(User).filter_by(**kwargs).first()
             if not user:
                 raise NoResultFound
             return user
